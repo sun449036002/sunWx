f0861623655ef960c14eb09cd817440d976a7b74
sun449036002 <449036002@qq.com>
2018-07-20 13:32:13 +0800

    edit

--- public/96.php
+++ public/96.php
@@ -26,6 +26,7 @@ $result = $obj->sign();
 
 <script>
 
+    $.ready(function(){
 wx.config({
             debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
             appId: '<?php echo $obj->appid; ?>', // 必填，公众号的唯一标识
@@ -38,14 +39,14 @@ wx.config({
 
             ] // 必填，需要使用的JS接口列表
         });
-		
-		
+        
+        
         //用ready方法来接收验证成功
         wx.ready(function() {
             // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
     
-	
-	        //分享到微信朋友圈
+    
+            //分享到微信朋友圈
             wx.onMenuShareTimeline({
                 title: '好好学习 天天向上', // 分享标题
                 link: '<?php echo $result["url"]; ?>', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
@@ -69,7 +70,9 @@ wx.config({
                      // 用户点击了分享后执行的回调函数
                 }
             });
-        })		
+        })
+    });
+    		
 
 
 
