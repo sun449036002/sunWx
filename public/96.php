3b12c8355586b342ac5ed9d7723683c43bd68782
sun449036002 <449036002@qq.com>
2018-07-20 13:25:20 +0800

    edit

--- public/96.php
+++ public/96.php
@@ -28,7 +28,7 @@ $result = $obj->sign();
 
 wx.config({
             debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-            appId: '<?php echo $result["appid"]; ?>', // 必填，公众号的唯一标识
+            appId: '<?php echo $obj["appid"]; ?>', // 必填，公众号的唯一标识
             timestamp: <?php echo $result["timestamp"]; ?>, // 必填，生成签名的时间戳
             nonceStr: '<?php echo $result["nonceStr"]; ?>', // 必填，生成签名的随机串
             signature: '<?php echo $result["signature"]; ?>',// 必填，签名
