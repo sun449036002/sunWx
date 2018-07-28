c024773664cea5d25a1d44996e579030cec822b3
sun449036002 <449036002@qq.com>
2018-07-28 11:00:57 +0800

    api wechat

---
+++ app/Http/Middleware/WeixinOAuth.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Consts\CookieConst;
+use Closure;
+use EasyWeChat\Factory;
+use Illuminate\Support\Facades\Cookie;
+
+class WeixinOAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        //未授权用户
+        if (empty($request->cookie(CookieConst::WECHAT_USER))) {
+
+            Cookie::queue('target_url', $request->getRequestUri(), 2);
+
+            $wxapp = Factory::officialAccount(getWxConfig());
+            return $wxapp->oauth->redirect();
+        }
+
+        return $next($request);
+    }
+}
