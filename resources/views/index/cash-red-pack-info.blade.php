baf1a95fc48204cbd4960e04940582cc3e4b199a
sun449036002 <449036002@qq.com>
2018-08-06 11:24:25 +0800

    api wechat animation

--- resources/views/index/cash-red-pack-info.blade.php
+++ resources/views/index/cash-red-pack-info.blade.php
@@ -5,34 +5,7 @@
 <script type="text/javascript" charset="utf-8">
     wx.config(<?php echo $wxapp->jssdk->buildConfig(['onMenuShareTimeline','onMenuShareAppMessage'], true) ?>);
 
-    //用ready方法来接收验证成功
-    wx.ready(function() {
-        // alert(location.href.split('#')[0]);
-        // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
 
-        //分享到微信朋友圈
-        wx.onMenuShareTimeline({
-            title: '分享到微信朋友圈，赚更多的现金', // 分享标题
-            link: "{{env('APP_URL')}}/assistance-page?redPackId={{$redPackId}}", // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-            imgUrl: 'http://www.gumama120.com/uploads/allimg/180531/1-1P531155024455.jpg', // 分享图标
-            success: function () {
-                // 用户点击了分享后执行的回调函数
-            }
-        });
-
-        //分享给微信好友
-        wx.onMenuShareAppMessage({
-            title: '分享给朋友们，赚更多的现金', // 分享标题
-            desc: '朋友点击支持后，双方都可获得赏金', // 分享描述
-            link: "{{env('APP_URL')}}/assistance-page?redPackId={{$redPackId}}", // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-            imgUrl: 'http://www.gumama120.com/uploads/allimg/180531/1-1P531155024455.jpg', // 分享图标
-            type: 'link', // 分享类型,music、video或link，不填默认为link
-            dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
-            success: function () {
-                // 用户点击了分享后执行的回调函数
-            }
-        });
-    });
 
     //滚动插件
     (function($) {
