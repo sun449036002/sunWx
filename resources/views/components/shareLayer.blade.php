1f8a64e63ef3352d75fcb12c970475fe6016947e
sun449036002 <449036002@qq.com>
2018-08-11 10:14:50 +0800

    红包配置获取

---
+++ resources/views/components/shareLayer.blade.php
@@ -0,0 +1,46 @@
+<style type="text/css">
+    .share-layer {
+        display: none;
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        background-color: rgba(0,0,0,0.66);
+    }
+
+    .share-layer img {
+        position: absolute;
+        top: .5rem;
+        right: 0;
+        width: 4rem;
+    }
+
+    .share-layer .share-tips {
+        position: absolute;
+        top: 3rem;
+        left: 0.375rem;
+        width: 6rem;
+        height: 2rem;
+        padding: 0.3rem;
+        border-radius: 0.1rem;
+        margin: auto;
+        background-color: white;
+        text-align: center;
+
+    }
+</style>
+<script>
+    $(document).ready(function(){
+        $(".share-layer").on("click", function(){
+            $(this).hide();
+        })
+    });
+</script>
+<div class="share-layer">
+    <img src="{{asset("imgs/jiantou.png")}}" />
+    <div class="share-tips">
+        <p>点击右上角按钮</p>
+        <p>邀请更多好友来助力吧~</p>
+    </div>
+</div>
