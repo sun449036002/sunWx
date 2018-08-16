39a54f8720a02391ac404ddfed6fae4051d35e14
sun449036002 <449036002@qq.com>
2018-08-16 08:25:12 +0800

    JS问题

--- resources/views/index/assistance-page.blade.php
+++ resources/views/index/assistance-page.blade.php
@@ -14,7 +14,7 @@
         wx.onMenuShareTimeline({
             title: '分享到微信朋友圈，赚更多的现金', // 分享标题
             link: '', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-            imgUrl: 'http://www.gumama120.com/uploads/allimg/180531/1-1P531155024455.jpg', // 分享图标
+            imgUrl: '{{asset('imgs/share-icon-1.png')}}', // 分享图标
             success: function () {
                 // 用户点击了分享后执行的回调函数
             }
@@ -25,7 +25,7 @@
             title: '分享给朋友们，赚更多的现金', // 分享标题
             desc: '朋友点击支持后，双方都可获得赏金', // 分享描述
             link: '', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-            imgUrl: 'http://www.gumama120.com/uploads/allimg/180531/1-1P531155024455.jpg', // 分享图标
+            imgUrl: '{{asset('imgs/share-icon-1.png')}}', // 分享图标
             type: 'link', // 分享类型,music、video或link，不填默认为link
             dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
             success: function () {
