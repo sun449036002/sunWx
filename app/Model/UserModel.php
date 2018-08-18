221cab266359f738ad8216080c055695f10e50dc
sun449036002 <449036002@qq.com>
2018-08-18 08:41:43 +0800

    时间

--- app/Model/UserModel.php
+++ app/Model/UserModel.php
@@ -16,7 +16,7 @@ class UserModel extends BaseModel
      * @return mixed
      */
     public function getUserinfoByOpenid($openid = '') {
-        $fields = ['id',"username", "is_subscribe", "type", "uri", "avatar_url", "admin_id"];
+        $fields = ['id',"username", "is_subscribe", "type", "uri", "avatar_url", "admin_id", "balance"];
         if (is_numeric($openid)) {
             $id = $openid;
             $row = $this->getOne($fields, ['id' => $id]);
