f6b1dc395498461e2fdd47982bf1bac14c5eb3f0
sun449036002 <449036002@qq.com>
2018-08-22 15:45:22 +0800

    路由

--- resources/views/index/cash-red-pack-info.blade.php
+++ resources/views/index/cash-red-pack-info.blade.php
@@ -11,8 +11,10 @@
         // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
 
         //分享到微信朋友圈
+        var shareTitle = "{{$this->user['username']}}邀请您帮他助力";
+        var shareDesc = "人多力量大，{{$total}}元待您领取";
         wx.onMenuShareTimeline({
-            title: '分享到微信朋友圈，赚更多的赏金', // 分享标题
+            title: shareTitle, // 分享标题
             link: "{{env('APP_URL')}}/assistance-page?redPackId={{$redPackId}}", // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
             imgUrl: '{{asset('imgs/share-icon-1.png')}}', // 分享图标
             success: function () {
@@ -23,8 +25,8 @@
 
         //分享给微信好友
         wx.onMenuShareAppMessage({
-            title: '分享给朋友们，赚更多的赏金', // 分享标题
-            desc: '朋友点击支持后，双方都可获得赏金', // 分享描述
+            title: shareTitle, // 分享标题
+            desc: shareDesc, // 分享描述
             link: "{{env('APP_URL')}}/assistance-page?redPackId={{$redPackId}}", // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
             imgUrl: '{{asset('imgs/share-icon-1.png')}}', // 分享图标
             type: 'link', // 分享类型,music、video或link，不填默认为link
