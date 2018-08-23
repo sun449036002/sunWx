9f1468bfb672eea99f3594f46a93396330c83292
sun449036002 <449036002@qq.com>
2018-08-23 14:39:26 +0800

    路由

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -30,3 +30,12 @@
             padding:0;
         }
     </style>
+
+    <script>
+        $(document).ready(function () {
+            document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
+                // 通过下面这个API隐藏右上角按钮
+                WeixinJSBridge.call('hideOptionMenu');
+            });
+        });
+    </script>
