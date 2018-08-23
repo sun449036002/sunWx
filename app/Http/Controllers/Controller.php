43cacd6e25f46ffde630e997267f1b2218611775
sun449036002 <449036002@qq.com>
2018-08-23 15:02:23 +0800

    路由

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -66,6 +66,9 @@ class Controller extends BaseController
         $this->middleware(function($request, $next){
             //获取用户信息
             $this->user = $this->getUserinfo();
+            echo "<pre>";
+            print_r($this->user);
+            exit;
 
             //wxapp对象
             $this->wxapp = Factory::officialAccount(getWxConfig());
