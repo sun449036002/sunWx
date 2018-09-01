5e38a92abbbc7435a273fc027774c631294eac43
Administrator <449036002@qq.com>
2018-09-01 14:08:41 +0800

    'edit'

--- app/Http/Controllers/MyController.php
+++ app/Http/Controllers/MyController.php
@@ -72,7 +72,7 @@ class MyController extends Controller
         ];
         $validate = Validator::make($data, $rule, $message);
         if (!$validate->passes()) {
-            return back()->withErrors($validate);
+            return ResultClientJson(100, json_encode($validate->getMessageBag()->getMessages(), JSON_UNESCAPED_UNICODE));
         }
 
         //返现金额打款账号
